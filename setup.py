63a830940e5391e67c7766069fc6fd5b0cbdea9a
tonio teran <toniioteran@gmail.com>
2025-07-28 23:11:52 -0700

    Use correct names in setup.py and deprecate old cmake lists.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'my_node = my_package.my_node:main'
+            'perception_node = lolo_perception.perception_node:main'
         ],
     },
 )
